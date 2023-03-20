53766a71d442bf8e8b9d49f7e56e749dea0a22e1
Hemant Kumar Yadav <hyadav@thesummitgrp.com>
2023-03-21 03:43:38 +0530

    again change

--- LiLTfinetune/data/data_args.py
+++ LiLTfinetune/data/data_args.py
@@ -73,7 +73,6 @@ class DataTrainingArguments:
         default=False,
         metadata={"help": "Whether to return all the entity levels during evaluation or just the overall ones."},
     )
-    data_dir: str = field(default=None, metadata={"help": "dir to dataset"})
 
 
 @dataclass
