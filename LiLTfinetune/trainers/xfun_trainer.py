8ab950c90d692b352989af9742e6c023e3ff6213
Hemant Kumar Yadav <hyadav@thesummitgrp.com>
2023-03-21 12:43:09 +0530

    asf

--- LiLTfinetune/trainers/xfun_trainer.py
+++ LiLTfinetune/trainers/xfun_trainer.py
@@ -42,7 +42,7 @@ class XfunReTrainer(FunsdTrainer):
         inputs = self._prepare_inputs(inputs)
 
         with torch.no_grad():
-            print(self)
+            print(self.cuda)
             if self.scaler.enabled:
                 with autocast():
                     outputs = model(**inputs)
